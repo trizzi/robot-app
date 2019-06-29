432800210f5bb90651bd6a0bb8a17bb16676127b
trizzi <33966004+trizzi@users.noreply.github.com>
2019-06-29 08:38:51 +0100

    Updates

---
+++ precache-manifest.c1188a14422d6850a3e62cb6691cf214.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d310a1afbfeed3216693fc496c57e783",
+    "url": "/robot-app/index.html"
+  },
+  {
+    "revision": "61e36ef5f7da0c8b3180",
+    "url": "/robot-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d69aaf5578c87f7cc7ae",
+    "url": "/robot-app/static/css/main.67942509.chunk.css"
+  },
+  {
+    "revision": "61e36ef5f7da0c8b3180",
+    "url": "/robot-app/static/js/2.d04e4f50.chunk.js"
+  },
+  {
+    "revision": "d69aaf5578c87f7cc7ae",
+    "url": "/robot-app/static/js/main.c3094547.chunk.js"
+  },
+  {
+    "revision": "d8c7712cb522ae4e063b",
+    "url": "/robot-app/static/js/runtime~main.ab1aa995.js"
+  }
+]);
